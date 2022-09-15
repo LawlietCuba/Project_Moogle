45110da56c636bf2fca8a0f747bf0119ea5cc669
LawlietCuba <rodrigao.mederos@gmail.com>
2022-09-14 22:18:32 -0400

    new commit

---
+++ MoogleEngine/Moogle.cs
@@ -0,0 +1,15 @@
+﻿namespace MoogleEngine;
+
+public class Moogle
+{
+    public static SearchResult Query(string query) {
+        
+        SearchItem[] items = new SearchItem[3] {
+            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
+            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
+            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
+        };
+
+        return new SearchResult(items, query);
+    }    
+}
